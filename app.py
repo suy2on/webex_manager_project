c1c69a1039e587ffd1888271d05c1cff4f90282c
suy2on <62363580+suy2on@users.noreply.github.com>
2020-11-12 02:17:13 +0900

    hw.html front-end
    to do : hw.html js and server

--- app.py
+++ app.py
@@ -20,14 +20,14 @@ def index():
 
 @app.route('/home')
 def home():
-    userid = session.get('userid', None)
-    print(userid['userid'])
-    return render_template('home.html', realuser=userid['userid'])
+    user = session.get('user', None)
+    print(user['userid'])
+    return render_template('home.html', realuser=user['userid'])
 
 @app.route('/classroom')
 def classroom():
-    userid = session.get('userid', None)
-    return render_template('class.html', realuser=userid['userid'])
+    user = session.get('user', None)
+    return render_template('class.html', realuser=user['userid'])
 
 @app.route('/join', methods=['GET'])
 def user_info():
@@ -77,9 +77,8 @@ def login():
     user = db.register.find_one({'userid': id_receive}, {'_id': False})
 
     if user['userpw']==pw_receive:
-        session['userid'] = user
-        print('true')
-        # return render_template('/home', realuser=user['userid'])
+        session['user'] = user #session에 로그인 한 사람의 register정보 저장
+
         return jsonify({'result': 'success', 'msg': '로그인 성공'})
     else:
         return jsonify({'result': 'false', 'msg': '아이디와 비밀번호가 일치하지 않습니다'})
@@ -88,7 +87,7 @@ def login():
 def add_class():
     class_receive=request.form['class_give']
     url_receive=request.form['url_give']
-    id = session.get('userid', None)['userid']
+    id = session.get('user', None)['userid']
 
     #db에서 add를 요청한 user의 기존 class data가져오기
     user = db.manage.find_one({'userid': id}, {'_id': False})
@@ -106,7 +105,7 @@ def add_class():
 @app.route('/class2', methods=['POST'])
 def delete_class():
     classname_receive=request.form['classname_give']
-    id = session.get('userid', None)['userid']
+    id = session.get('user', None)['userid']
 
     #db에서 요청한 유저의 기존데이터가져오기
     user = db.manage.find_one({'userid': id}, {'_id': False})
@@ -123,7 +122,7 @@ def delete_class():
 
 @app.route('/class', methods=['GET'])
 def show_class():
-    id = session.get('userid', None)['userid']
+    id = session.get('user', None)['userid']
     #db에서 가져와서 넘겨주기
     user = db.manage.find_one({'userid': id}, {'_id': False})
     print(user)
@@ -133,14 +132,5 @@ def show_class():
     return jsonify({'result': 'success', 'classname': classname, 'classurl': classurl})
 
 if __name__ == '__main__':
-    #dbfile = os.path.join(basedir, 'db.sqlite')  # 데이터베이스 이름과 경로
-    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
-    #app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True  # 사용자에게 원하는 정보를 전달완료했을때가 TEARDOWN, 그 순간마다 COMMIT을 하도록 한다.라는 설정
-    # 여러가지 쌓아져있던 동작들을 Commit을 해주어야 데이터베이스에 반영됨. 이러한 단위들은 트렌젝션이라고함.
-    #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # True하면 warrnig메시지 유발,
-
-    #db.init_app(app)  # 초기화 후 db.app에 app으로 명시적으로 넣어줌
-    #db.app = app
-    #db.create_all()  # 이 명령이 있어야 생성됨. DB가
     app.config['SECRET_KEY'] = 'wcsfeufhwiquehfdx'
     app.run('0.0.0.0', port=5000, debug=True)
